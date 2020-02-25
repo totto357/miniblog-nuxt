3a31a62565aeac4b2cfb84a0fa81a64fad5e9c99
totto357 <totto357@gmail.com>
2020-02-26 02:10:02 +0900

    chore プロジェクトの基本ファイルを追加

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import { Configuration } from "@nuxt/types"
+
+const config: Configuration = {
+  mode: "spa",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  plugins: [
+    "~/plugins/firebase",
+  ],
+  buildModules: [
+    "@nuxtjs/vuetify",
+    "@nuxt/typescript-build",
+  ],
+}
+
+export default config
