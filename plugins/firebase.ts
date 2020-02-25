61161c1f26f0df318a354b02d3a89869ad78227d
totto357 <totto357@gmail.com>
2020-02-26 02:10:32 +0900

    feat firebaseをプラグイン化

---
+++ plugins/firebase.ts
@@ -0,0 +1,25 @@
+import firebase, { User } from "firebase"
+import { authModule } from "@/store"
+
+const config = {
+  apiKey: "AIzaSyBlL6jeJByze2AiHP6a47aWhpVv4uFKrsE",
+  authDomain: "nuxt-ts.firebaseapp.com",
+  databaseURL: "https://nuxt-ts.firebaseio.com",
+  projectId: "nuxt-ts",
+  storageBucket: "nuxt-ts.appspot.com",
+  messagingSenderId: "217016186100",
+  appId: "1:217016186100:web:5f04ec9cda088601881aa1"
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
+
+export default () => {
+  auth.onAuthStateChanged((user: User | null) => {
+    authModule.onAuthStateChanged(user)
+  })
+}
